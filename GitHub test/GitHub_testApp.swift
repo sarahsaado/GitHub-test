e1f10d17432eb172cffbdc1573fa3e52ce48db7e
xadiasmoon <ssaado2000@gmail.com>
2023-06-26 10:30:43 -0400

    Initial Commit

---
+++ GitHub test/GitHub_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_testApp.swift
+//  GitHub test
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
